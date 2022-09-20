8783978059e94c5240febac55bce2e396a1fd08f
palvin254 <palvinpaul900m@gmail.com>
2022-09-20 19:46:50 +0000

    python

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
-for i in range(122, 96, -1):
-    if i % 2 != 0:
-        print("{:c}".format(i - 32), end='')
-    else:
-        print("{:c}".format(i), end='')
+def remove_char_at(str, n):
+    part = str[:n]
+    parts = str[n+1:]
+    return part + parts
